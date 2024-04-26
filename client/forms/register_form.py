a60d2e1c06045ddc2944e73df339950835f06ef4
yyb <90058178+YuanYubin1@users.noreply.github.com>
2024-04-26 21:48:55 +0800

    Add files via upload

--- client/forms/register_form.py
+++ client/forms/register_form.py
@@ -53,9 +53,10 @@ class RegisterForm(tk.Frame):
         self.user_name = tk.Label(self.master, text="用户名", font=("微软雅黑", 16), fg="black", bg="#30cfe8")
         self.user_pwd = tk.Label(self.master, text="密  码", font=("微软雅黑", 16), fg="black", bg="#32d0ea")
         self.confirm_pwd = tk.Label(self.master, text="确认密码", font=("微软雅黑", 16), fg="black", bg="#3ad2e9")
-        self.user_email = tk.Label(self.master, text="邮  箱", font=("微软雅黑", 16), fg="black", bg="#58daec")
+        self.user_school_id = tk.Label(self.master, text="学/工号", font=("微软雅黑", 16), fg="black", bg="#58daec")
         self.user_sex = tk.Label(self.master, text="性  别", font=("微软雅黑", 16), fg="black", bg="#5bdbec")
         self.user_age = tk.Label(self.master, text="年  龄", font=("微软雅黑", 16), fg="black", bg="#5bdbec")
+        self.user_role = tk.Label(self.master, text="角  色", font=("微软雅黑", 16), fg="black", bg="#5bdbec")
         # 输入框
         # 用户名输入框
         self.var_user_name = tk.StringVar()
@@ -69,9 +70,9 @@ class RegisterForm(tk.Frame):
         self.var_confirm_pwd = tk.StringVar()
         self.entry_confirm_pwd = tk.Entry(self.master, textvariable=self.var_confirm_pwd, show='* ', font=("微软雅黑", 18),
                                           fg="black", bg="#54d9ec", relief=GROOVE)
-        # 邮箱输入框
-        self.var_user_email = tk.StringVar()
-        self.entry_user_email = tk.Entry(self.master, textvariable=self.var_user_email, font=("Arial", 14), fg="black",
+        # 学/工号输入框
+        self.var_user_school_id = tk.StringVar()
+        self.entry_user_school_id = tk.Entry(self.master, textvariable=self.var_user_school_id, font=("Arial", 14), fg="black",
                                          bg="#59d8ee", relief=GROOVE)
         # 性别输入框
         self.var_user_sex = tk.StringVar()
@@ -90,6 +91,12 @@ class RegisterForm(tk.Frame):
                                          81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
                                          100)
         self.entry_user_age.current(0)
+        # 角色输入框
+        self.var_user_role = tk.StringVar()
+        self.entry_user_role = ttk.Combobox(self.master, textvariable=self.var_user_role, font=("微软雅黑", 18),
+                                           state="readonly")
+        self.entry_user_role['values'] = ("学生", "教师")
+        self.entry_user_role.current("0")
         # 注册按钮
         self.register_btn = tk.Button(self.master, text='       注     册       ', font=("微软雅黑", 18), fg="black",
                                       bg="#5edbee", activebackground="#61d8ee", relief=GROOVE, width=30,
@@ -100,18 +107,20 @@ class RegisterForm(tk.Frame):
         self.user_name.grid(row=8, column=1, columnspan=2, sticky=E)
         self.user_pwd.grid(row=9, column=1, columnspan=2, sticky=E)
         self.confirm_pwd.grid(row=10, column=1, columnspan=2, sticky=E)
-        self.user_email.grid(row=11, column=1, columnspan=2, sticky=E)
+        self.user_school_id.grid(row=11, column=1, columnspan=2, sticky=E)
         self.user_sex.grid(row=12, column=1, columnspan=2, sticky=E)
         self.user_age.grid(row=13, column=1, columnspan=2, sticky=E)
+        self.user_role.grid(row=14, column=1, columnspan=2, sticky=E)
         # 输入框位置定位
         self.entry_user_name.grid(row=8, column=3, columnspan=3, sticky=W, )
         self.entry_user_pwd.grid(row=9, column=3, columnspan=3, sticky=W, )
         self.entry_confirm_pwd.grid(row=10, column=3, columnspan=3, sticky=W, )
-        self.entry_user_email.grid(row=11, column=3, columnspan=3, sticky=W, )
+        self.entry_user_school_id.grid(row=11, column=3, columnspan=3, sticky=W, )
         self.entry_user_sex.grid(row=12, column=3, columnspan=3, sticky=W, )
         self.entry_user_age.grid(row=13, column=3, columnspan=3, sticky=W, )
+        self.entry_user_role.grid(row=14, column=3, columnspan=3, sticky=W, )
         # 注册按钮位置定位
-        self.register_btn.grid(row=14, column=2, columnspan=4, sticky=S + W)
+        self.register_btn.grid(row=15, column=2, columnspan=4, sticky=S + W)
         # 设置下拉框背景
         self.combo_style = tk.ttk.Style()
         self.combo_style.theme_create('self.combo_style', parent='alt',
@@ -140,13 +149,13 @@ class RegisterForm(tk.Frame):
         password = self.var_user_pwd.get()
         #print(type(password).__name__)
         password_confirmation = self.var_confirm_pwd.get()
-        email = self.var_user_email.get()
-        #print(type(email).__name__)
+        school_id = self.var_user_school_id.get()
+        #print(type(school_id).__name__)
         sex = self.var_user_sex.get()
         #print(type(sex).__name__)
         age = self.var_user_age.get()
         #print(type(age).__name__)
-
+        role = self.var_user_school_id.get()
         ip = get_ip()
         #print(type(ip).__name__)
         config = get_config()
@@ -156,8 +165,8 @@ class RegisterForm(tk.Frame):
         if not username:
             messagebox.showerror("Error", "用户名不能为空")
             return
-        if not email:
-            messagebox.showerror("Error", "邮箱不能为空")
+        if not school_id:
+            messagebox.showerror("Error", "学/工号不能为空")
             return
         if not password:
             messagebox.showerror("Error", "密码不能为空")
@@ -165,10 +174,10 @@ class RegisterForm(tk.Frame):
         if password != password_confirmation:
             messagebox.showerror("Error", "两次密码输入不一致")
             return
-        if not re.match(r'^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[0-9a-zA-Z]{1,3}$',email):
-            messagebox.showerror("Error", "邮箱格式错误")
+        if not re.match(r'^[0-9]{0,8}$',school_id):
+            messagebox.showerror("Error", "学工/号格式错误")
             return
-        self.sc.send(MessageType.register, [username, password, email, ip, port, sex, age])
+        self.sc.send(MessageType.register, [username, password, school_id, ip, port, sex, age, role])
 
         certname = ip + "_cert.pem"
         with open(certname, 'r') as f:
@@ -177,7 +186,7 @@ class RegisterForm(tk.Frame):
             sp = context.split(' ')
             f.close()
         with open(certname,'w') as f:
-            f.write((str(self.var_user_name.get()) + ' ' + str(self.var_user_email.get()) + " " + str(sp[2])))
+            f.write((str(self.var_user_name.get()) + ' ' + str(self.var_user_school_id.get()) + " " + str(sp[2])))
             f.close()
         with open(certname, "r") as f:
             a = f.read()
