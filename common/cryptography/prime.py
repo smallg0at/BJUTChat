7e7bf078adf5661b24d204174a94b4063bce09c9
YC L <smallg0at@outlook.com>
2024-05-09 11:06:23 +0800

    throw in sympy

--- common/cryptography/prime.py
+++ common/cryptography/prime.py
@@ -6,24 +6,9 @@
 先生成随机数，然后判断是否是素数
 """
 
-from random import randint
-
-""" 判断是否为大素数 """
-def is_prime(num, test_count):
-    if num == 1:
-        return False
-    if test_count >= num:
-        test_count = num - 1
-    for x in range(test_count):
-        val = randint(1, num - 1)
-        if pow(val, num-1, num) != 1:
-            return False
-    return True
+# from random import randint
+import sympy
 
 """ 生成大素数 """
 def generate_big_prime(n):
-    found_prime = False
-    while not found_prime:
-        p = randint(2**(n-1), 2**n)
-        if is_prime(p, 1000):
-            return p
+    return sympy.randprime(2048, 4096)
