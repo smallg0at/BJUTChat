85c0f6b0e44360023deae90bd1800e4cb8d9563e
YC L <smallg0at@outlook.com>
2024-05-19 21:59:31 +0800

    fix

--- common/cryptography/crypt.py
+++ common/cryptography/crypt.py
@@ -19,29 +19,29 @@ def gen_secret():
 
     private_self = PrivateKey.generate()
     public_self = private_self.public_key
-    with open("private.pem", "wb") as f:
-        f.write(private_self)
+    with open("private.pem", "w") as f:
+        f.write(private_self.encode(nacl.encoding.Base64Encoder))
         f.close()
-    with open("public.pem", "wb") as f:
-        f.write(public_self)
+    with open("public.pem", "w") as f:
+        f.write(public_self.encode(nacl.encoding.Base64Encoder))
         f.close()
 
     
 
-"""生成共享密钥"""
-def get_shared_secret(their_secret):
+# """生成共享密钥"""
+# def get_shared_secret(their_secret):
 
-    with open("public.pem", "rb") as f:
-        pub = f.read()
-        f.close()
+#     with open("public.pem", "rb") as f:
+#         pub = f.read()
+#         f.close()
 
-    f = open("private.pem", "rb")
-    secret = int(f.read())
-    f.close()
-    return hashlib.sha256(long_to_bytes(int(their_secret) ** secret % modulus)).digest()
+#     f = open("private.pem", "rb")
+#     secret = int(f.read())
+#     f.close()
+#     return hashlib.sha256(long_to_bytes(int(their_secret) ** secret % modulus)).digest()
 
-"""从证书中获取公钥"""
-def getpk_from_cert(cert):
+# """从证书中获取公钥"""
+# def getpk_from_cert(cert):
 
-    str = cert.split()
-    return str[2]
+#     str = cert.split()
+#     return str[2]
