c0c386f647fbb66dd52ac0aab155d044e0aecca1
YC L <smallg0at@outlook.com>
2024-07-12 13:11:14 +0800

    revamped loginui

--- client/__init__.py
+++ client/__init__.py
@@ -4,12 +4,15 @@
 """运行客户端"""
 import _thread
 import tkinter as tk
+from tkinter import ttk
+import sv_ttk
 import ctypes
 from tkinter import messagebox
 import client.memory
 import client.util.socket_listener
 from client.forms.login_form import LoginForm
 from common.transmission.secure_channel import establish_secure_channel_to_server
+from ttkthemes import ThemedTk
 
 """运行客户端开启一个新的线程"""
 def run():
@@ -26,6 +29,8 @@ def run():
     # 设置程序缩放
     client.memory.tk_root.tk.call('tk', 'scaling', ScaleFactor/75)
     
+    sv_ttk.set_theme('light')
+    
     try:
         client.memory.sc = establish_secure_channel_to_server()
     except ConnectionError:
@@ -34,8 +39,10 @@ def run():
 
     _thread.start_new_thread(client.util.socket_listener.socket_listener_thread, (client.memory.sc, root))
     login = tk.Toplevel()
+    
     LoginForm(master=login)
     root.withdraw()
+    
     root.mainloop()
     try:
         root.destroy()
--- client/forms/login_form.py
+++ client/forms/login_form.py
@@ -5,11 +5,11 @@
 import _tkinter
 import sys
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from common.message import MessageType
 from pprint import pprint
 import client.memory
-from client.forms.register_form import RegisterForm
 from client.forms.contacts_form import ContactsForm
 import select
 import _thread
@@ -17,78 +17,228 @@ import os
 from tkinter import *
 from tkinter import Toplevel
 import client.util.socket_listener
+import sv_ttk
+from common.config import get_config
+import re
+
 
 """登录界面"""
+
+
 class LoginForm(tk.Frame):
-    """ 关闭端口监听 """
+    """关闭端口监听"""
+
     def remove_socket_listener_and_close(self):
         client.util.socket_listener.remove_listener(self.socket_listener)
         self.master.destroy()
 
     """" 关闭窗口 """
+
     def destroy_window(self):
         client.memory.tk_root.destroy()
 
     """ 开启监听 """
+
     def socket_listener(self, data):
-        if data['type'] == MessageType.login_failed:
-            messagebox.showerror('Error', '登入失败，请检查用户名密码')
+        if data["type"] == MessageType.login_failed:
+            messagebox.showerror("Error", "登入失败，请检查用户名密码")
             return
 
-        if data['type'] == MessageType.login_successful:
-            client.memory.current_user = data['parameters']
+        if data["type"] == MessageType.login_successful:
+            client.memory.current_user = data["parameters"]
             self.remove_socket_listener_and_close()
             print("Login Successful!")
             contacts = Toplevel(client.memory.tk_root, takefocus=True)
             ContactsForm(contacts)
             return
-        
+    
+        if data['type'] == MessageType.username_taken:
+            messagebox.showerror('Error', '用户名已被使用，请换一个')
+            return
+
+        if data['type'] == MessageType.register_successful:
+            messagebox.showinfo('Congratulations', '注册成功! ')
+            # self.remove_socket_listener_and_close()
+            return
+
     def __init__(self, master=None):
         """创建主窗口用来容纳其他组件"""
         super().__init__(master)
+        # sv_ttk.set_theme('dark')
+        style = ttk.Style()
+        style.configure(".", font=("微软雅黑", 12))
+        
         self.master = master
-        self.master.title("登录")
-        self.master.resizable(width=False, height=False)
-        self.master.geometry('960x600')
-        # 画布放置图片
-        self.canvas = tk.Canvas(self.master, width=960, height=600)
-        # self.image_file = tk.PhotoImage(file='client/forms/images/login_bg.gif')
-        # self.image = self.canvas.create_image(0, 0, anchor='nw', image=self.image_file)
+        self.master.title("BJUTChat")
+        # self.master.resizable(width=False, height=False)
+        self.master.geometry("600x800")
+
+        self.notebook = ttk.Notebook(self.master)
+        self.notebook.pack(expand=True, fill='both')
+
+        self.login_frame = ttk.Frame(self.notebook, width=600, height=800)
+        self.register_frame = ttk.Frame(self.notebook, width=600, height=800)
+        
+
+
+
+
+        # Login
+        # self.canvas = tk.Canvas(self.login_frame, width=600, height=400, background="#d9d9d9", highlightthickness=0)
         # 标签 用户名密码
-        self.user_name = tk.Label(self.master, text='用户名', font=("微软雅黑", 16), )
-        self.user_pwd = tk.Label(self.master, text='密码', font=("微软雅黑", 16), )
+        self.login_user_name = ttk.Label(self.login_frame, text="用户名",justify='left', width=26)
+        self.login_user_pwd = ttk.Label(self.login_frame, text="密码",justify='left', width=26)
         # 用户名输入框
-        self.var_user_name = tk.StringVar()
-        self.entry_user_name = tk.Entry(self.master, textvariable=self.var_user_name, font=("微软雅黑", 18), relief=GROOVE)
+        self.var_login_user_name = tk.StringVar()
+        self.login_entry_user_name = ttk.Entry(
+            self.login_frame,
+            textvariable=self.var_login_user_name,
+            font=("微软雅黑", 12),
+            width=25,
+            justify="center"
+        )
         # 密码输入框
-        self.var_user_pwd = tk.StringVar()
-        self.entry_user_pwd = tk.Entry(self.master, textvariable=self.var_user_pwd, show='* ', font=("微软雅黑", 18),
-                                       relief=GROOVE)
+        self.login_var_user_pwd = tk.StringVar()
+        self.login_entry_user_pwd = ttk.Entry(
+            self.login_frame,
+            textvariable=self.login_var_user_pwd,
+            show="* ",
+            font=("微软雅黑", 12),
+            width=25,
+            justify="center",
+        )
         # 登录 注册按钮
-        self.register_btn = tk.Button(self.master, text='注册', font=("微软雅黑", 18), fg="black",  relief=GROOVE, command=self.show_register)
-        self.login_btn = Button(self.master, text='登录', font=("微软雅黑", 18), fg="black",  relief=GROOVE, command=self.do_login)
-        self.quit_btn = tk.Button(self.master, text='退出', font=("微软雅黑", 18), fg="black",  relief=GROOVE, command=self.destroy_window)
+
+        self.login_btn = ttk.Button(
+            self.login_frame,
+            text="登录",
+            command=self.do_login,
+            width=25,
+            style="Accent.TButton",
+        )
+
+        # self.pack(expand=True, fill=BOTH)
         # 位置定位
-        self.canvas.grid(row=0, column=0, rowspan=5, columnspan=8, )
-        self.user_name.grid(row=1, column=2, sticky=E, padx=0, pady=0, )
-        self.user_pwd.grid(row=2, column=2, sticky=E + N)
-        self.entry_user_name.grid(row=1, column=3, columnspan=2, sticky=W)
-        self.entry_user_pwd.grid(row=2, column=3, columnspan=2, sticky=W + N)
-        self.register_btn.grid(row=3, column=2, columnspan=1, sticky=E + N)
-        self.login_btn.grid(row=3, column=3, columnspan=1, sticky=E + N)
-        self.quit_btn.grid(row=3, column=4, columnspan=1, sticky=E + N)
+        # self.canvas.grid(
+        #     row=0,
+        #     column=0,
+        #     rowspan=6,
+        #     columnspan=2,
+        # )
+        for i in range(0,6):
+            self.login_frame.rowconfigure(i, weight=0, pad=15, minsize=20)
+        
+
+        self.login_frame.columnconfigure(0, weight=3)
+
+
+        self.login_user_name.grid(
+            row=1,
+            column=0,
+            columnspan=2,
+            sticky=N,
+            padx=0,
+            pady=0,
+        )
+        self.login_entry_user_name.grid(row=2, column=0, columnspan=1, sticky=N)
+        self.login_user_pwd.grid(row=3, column=0, columnspan=1, sticky=N)
+        self.login_entry_user_pwd.grid(row=4, column=0, columnspan=1, sticky=N)
+        self.login_btn.grid(row=6, column=0, columnspan=1, sticky=N)
+        # self.register_btn.grid(row=7, column=0, columnspan=2, sticky=N)
+        # self.quit_btn.grid(row=8, column=0, columnspan=2, sticky=N)
+
+
+        # Register
+        # 画布
+        # self.canvas = tk.Canvas(self.register_frame, width=600, height=1000, background="#d9d9d9", highlightthickness=0)
+        # 标签 用户名、密码、确认密码、邮箱、性别、年龄
+        self.reg_user_name = ttk.Label(self.register_frame, text="用户名 ")
+        self.reg_user_pwd = ttk.Label(self.register_frame, text="密码 ")
+        self.confirm_pwd = ttk.Label(self.register_frame, text="确认密码 ")
+        self.user_school_id = ttk.Label(self.register_frame, text="学工号 ")
+        self.user_sex = ttk.Label(self.register_frame, text="性别 ")
+        self.user_age = ttk.Label(self.register_frame, text="年龄 ")
+        self.user_role = ttk.Label(self.register_frame, text="角色 ")
+        # 输入框
+        # 用户名输入框
+        self.var_reg_user_name = tk.StringVar()
+        self.reg_entry_user_name = ttk.Entry(self.register_frame, textvariable=self.var_reg_user_name, font=("微软雅黑", 12))
+        # 密码输入框
+        self.reg_var_user_pwd = tk.StringVar()
+        self.reg_entry_user_pwd = ttk.Entry(self.register_frame, textvariable=self.reg_var_user_pwd, show='* ', font=("微软雅黑", 12))
+        # 确认密码输入框
+        self.var_confirm_pwd = tk.StringVar()
+        self.entry_confirm_pwd = ttk.Entry(self.register_frame, textvariable=self.var_confirm_pwd, show='* ', font=("微软雅黑", 12))
+        # 学/工号输入框
+        self.var_user_school_id = tk.StringVar()
+        self.entry_user_school_id = ttk.Entry(self.register_frame, textvariable=self.var_user_school_id,font=("微软雅黑", 12))
+        # 性别输入框
+        self.var_user_sex = tk.StringVar()
+        self.entry_user_sex = ttk.Combobox(self.register_frame, textvariable=self.var_user_sex, font=("微软雅黑", 12),
+                                           state="readonly")
+        self.entry_user_sex['values'] = ("保密", "男", "女")
+        self.entry_user_sex.current(0)
+
+        # 角色输入框
+        self.var_user_role = tk.StringVar()
+        self.entry_user_role = ttk.Combobox(self.register_frame, textvariable=self.var_user_role, font=("微软雅黑", 12),
+                                           state="readonly")
+        self.entry_user_role['values'] = ("学生", "教师")
+        self.entry_user_role.current("0")
+        # 注册按钮
+        self.register_btn = ttk.Button(self.register_frame, text='注册',
+                                       width=25, style="Accent.TButton",
+                                      command=self.do_register)
+        # 位置定位
+        # label位置定位
+        # self.canvas.grid(row=0, column=0, rowspan=16, columnspan=8, )
+        for i in range(0,8):
+            self.register_frame.rowconfigure(i, weight=0, pad=15, minsize=20)
+        
+        
+        self.register_frame.columnconfigure(0, weight=0, pad=15, minsize=20)
+        self.register_frame.columnconfigure(1, weight=0, pad=15)
+        self.register_frame.columnconfigure(2, weight=5, pad=15)
+        self.register_frame.columnconfigure(3, weight=0, pad=15, minsize=20)
+        
+        
+        self.reg_user_name.grid(row=1, column=1, sticky=E)
+        self.reg_user_pwd.grid(row=2, column=1, sticky=E)
+        self.confirm_pwd.grid(row=3, column=1, sticky=E)
+        self.user_school_id.grid(row=4, column=1, sticky=E)
+        self.user_sex.grid(row=5, column=1, sticky=E)
+        self.user_role.grid(row=6, column=1, sticky=E)
+        # 输入框位置定位
+        self.reg_entry_user_name.grid(row=1, column=2, sticky='we', )
+        self.reg_entry_user_pwd.grid(row=2, column=2, sticky='we', )
+        self.entry_confirm_pwd.grid(row=3, column=2, sticky='we', )
+        self.entry_user_school_id.grid(row=4, column=2, sticky='we', )
+        self.entry_user_sex.grid(row=5, column=2, sticky='we', )
+        self.entry_user_role.grid(row=6, column=2, sticky='we', )
+        # 注册按钮位置定位
+        self.register_btn.grid(row=7, column=1, columnspan=2, sticky=S)
+
+
+        self.notebook.add(self.login_frame, text="登录")
+        self.notebook.add(self.register_frame, text="注册")
+
         self.sc = client.memory.sc
+
         client.util.socket_listener.add_listener(self.socket_listener)
 
         self.master.protocol("WM_DELETE_WINDOW", self.destroy_window)
 
     """ 登陆操作 """
+
     def do_login(self):
         """登录操作若为空则提示用户错误"""
-        username = self.var_user_name.get()
-        password = self.var_user_pwd.get()
+        username = self.var_login_user_name.get()
+        password = self.login_var_user_pwd.get()
         if not username:
-            messagebox.showerror("Error", "用户名不能为空", )
+            messagebox.showerror(
+                "Error",
+                "用户名不能为空",
+            )
             return
         if not password:
             messagebox.showerror("Error", "密码不能为空")
@@ -96,6 +246,44 @@ class LoginForm(tk.Frame):
         self.sc.send(MessageType.login, [username, password])
 
     """ 转到注册界面 """
+
     def show_register(self):
         register_form = Toplevel()
         RegisterForm(register_form)
+
+    """" 注册操作 """
+    def do_register(self):
+
+        username = self.var_login_user_name.get()
+       # print(type(username).__name__)
+        password = self.login_var_user_pwd.get()
+        #print(type(password).__name__)
+        password_confirmation = self.var_confirm_pwd.get()
+        school_id = self.var_user_school_id.get()
+        #print(type(school_id).__name__)
+        sex = self.var_user_sex.get()
+        #print(type(sex).__name__)
+        # age = self.var_user_age.get()
+        #print(type(age).__name__)
+        role = self.var_user_school_id.get()
+        #print(type(ip).__name__)
+        config = get_config()
+        port = str((config['client']['client_port']))
+        #print(type(port).__name__)
+
+        if not username:
+            messagebox.showerror("Error", "用户名不能为空")
+            return
+        if not school_id:
+            messagebox.showerror("Error", "学/工号不能为空")
+            return
+        if not password:
+            messagebox.showerror("Error", "密码不能为空")
+            return
+        if password != password_confirmation:
+            messagebox.showerror("Error", "两次密码输入不一致")
+            return
+        if not re.match(r'^[0-9]{0,8}$',school_id):
+            messagebox.showerror("Error", "学工/号格式错误")
+            return
+        self.sc.send(MessageType.register, [username, password, school_id, sex, role])
