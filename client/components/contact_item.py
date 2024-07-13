02cb1d5f1ada94863140b047075f9eed789136de
YC L <smallg0at@outlook.com>
2024-07-13 13:54:58 +0800

    overhaul

--- client/components/contact_item.py
+++ client/components/contact_item.py
@@ -3,50 +3,49 @@
 
 """联系人列表 UI"""
 from tkinter import *
+from tkinter import ttk 
 from pprint import pprint
 
 """"联系人界面"""
-class ContactItem(Frame):
+class ContactItem(ttk.Frame):
 
     def __init__(self, parent, onclick):
-        Frame.__init__(self, parent)
+        ttk.Frame.__init__(self, parent)
 
         def handle_on_click(e):
             e.widget = self
             onclick(e)
 
-        Frame.config(self, borderwidth=2, relief=GROOVE)
-
+        # ttk.Frame.config(self, borderwidth=2, style="Contact.TFrame")
+        ttk.Frame.config(self, borderwidth=2, style="Contact1.TFrame")
+        ttk.Separator(self, orient='horizontal', style="Contact.TSeparator").pack(side='bottom', expand=True, fill='x', pady=10)
         # == Line 1
-        self.title_frame = Frame(self)
-        self.last_message_time_frame = Frame(self)
-        self.friend_ip_frame = Frame(self)
-        self.message_frame = Frame(self)
+        self.title_frame = ttk.Frame(self, style="Contact.TFrame")
+        self.last_message_time_frame = ttk.Frame(self, style="Contact.TFrame")
+        self.message_frame = ttk.Frame(self, style="Contact.TFrame")
 
         # last_message
         self.message_frame.pack(side=BOTTOM, fill=BOTH, expand=True, pady=(0, 5), padx=3)
-        self.last_message = Label(self.message_frame, text="recent message", font=('新宋体', 13), fg='black')
+        self.last_message = Label(self.message_frame, text="recent message", font=('微软雅黑', 13), fg='black', bg="#f0f0f0")
         self.last_message.pack(side=LEFT, fill=X, expand=True, anchor=W)
 
         # title
         self.title_frame.pack(side=LEFT, fill=BOTH, expand=True, anchor=W, pady=(1, 1), padx=3)
-        self.title = Label(self.title_frame, text="Title", font=("微软雅黑", 15, 'bold'))
-        self.title.pack(side=LEFT, fill=None, anchor=W)
+        self.title = Label(self.title_frame, text="Title", font=("微软雅黑", 15, 'bold'), bg="#f0f0f0")
+        self.title.pack(side=LEFT, fill='x', anchor=W)
 
         # 最后一条消息的时间
         self.last_message_time_frame.pack(side=TOP, expand=FALSE, fill=BOTH)
-        self.last_message_time = Label(self.last_message_time_frame, text="date", font=('微软雅黑', 10), fg='#575757')
+        self.last_message_time = Label(self.last_message_time_frame, text="date", font=('微软雅黑', 9), fg='#575757', bg="#f0f0f0")
         self.last_message_time.pack(side=RIGHT, anchor=E)
 
-        # 好友的ip
-        self.friend_ip_frame.pack(side=TOP, expand=FALSE, fill=BOTH)
-        self.friend_ip = Label(self.friend_ip_frame, text="ip", font=('微软雅黑', 10), fg='#575757')
-        self.friend_ip.pack(side=RIGHT, anchor=E)
 
         # 处理未读消息
         self.unread_message_count = Label(self.message_frame, text="0", font=('Arial', 10), fg='white', bg='red')
         self.unread_message_count.pack(side=RIGHT, anchor=E, fill=None, expand=False, ipadx=2)
         self.unread_message_count.pack_forget()
+
+        
 
         # 将单击事件传输到父级
         self.title.bind("<Button>", handle_on_click)
