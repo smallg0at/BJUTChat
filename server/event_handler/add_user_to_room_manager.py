c68eb9e7ce6606522e11316dc2ba60819a0f3f6b
Yuanyubin1 <1426796968@qq.com>
2024-07-14 23:29:44 +0800

    支持管理员相关功能

---
+++ server/event_handler/add_user_to_room_manager.py
@@ -0,0 +1,28 @@
+# #!/usr/bin/env python
+# # -*- coding:utf-8 -*-
+
+from pprint import pprint
+from common.message import MessageType
+from server.broadcast import broadcast
+import server.memory
+from common.util import md5
+from server.util import database
+from server.util import add_target_type
+
+
+def run(sc, parameters):
+    #parameters = [user_id,room_id]
+    user_id = parameters[0]
+    room_id = parameters[1]
+    operator_id = server.memory.sc_to_user_id[sc]
+    #身份检查,操作者必须为群创建者,被设为管理员的用户必须在群内
+    if(database.is_room_creator(operator_id, room_id)):
+        if(database.in_room(user_id, room_id)):    
+            database.add_user_to_room_manager(user_id, room_id)
+            sc.send(MessageType.add_user_to_room_manager_result, [True, user_id, room_id])
+        else: sc.send(MessageType.general_failure, '该用户必须在群内才能被设为管理员')
+    else: 
+        sc.send(MessageType.general_failure, '只有群主才能把用户设为管理员')
+
+    
+
