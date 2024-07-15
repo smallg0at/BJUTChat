6ff8f75699f561c47dd23370eba0ce97166ed96e
bigwhite <l18888852465@163.com>
2024-07-15 23:20:07 +0800

    add upload record and download record

--- file_server/init_db.py
+++ file_server/init_db.py
@@ -1,13 +1,23 @@
 import sqlite3
+import os
+# 防呆设计
+if not os.path.exists('file_server/database.db'):
+    print("You're probably on the wrong working directory!")
+
 
 # 连接到数据库
 conn = sqlite3.connect('file_server/database.db')
+
+if not os.path.exists('file_server'):
+    os.makedirs('file_server')
 
 # 创建一个游标对象
 cur = conn.cursor()
 
 # 删除现有的文件表（如果存在）
 cur.execute('''DROP TABLE IF EXISTS "main"."files"''')
+cur.execute('''DROP TABLE IF EXISTS "upload_logs"''')
+cur.execute('''DROP TABLE IF EXISTS "download_logs"''')
 
 # 创建一个名为files的表
 cur.execute('''
@@ -16,6 +26,24 @@ CREATE TABLE "files" (
     "upload_time" INTEGER,
     "is_deleted" BOOLEAN,
     PRIMARY KEY ("id")
+)
+''')
+
+cur.execute('''
+CREATE TABLE "upload_logs" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "file_id" TEXT,
+    "user_id" TEXT,
+    "timestamp" INTEGER
+)
+''')
+
+cur.execute('''
+CREATE TABLE "download_logs" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "file_id" TEXT,
+    "user_id" TEXT,
+    "timestamp" INTEGER
 )
 ''')
 
